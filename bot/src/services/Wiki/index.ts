71bb1f5a8412bf768f08471964855f70c0682957
etwas <etwas@example.com>
2024-07-24 04:52:30 +0900

    ✨ Fix Wiki Description

--- bot/src/services/Wiki/index.ts
+++ bot/src/services/Wiki/index.ts
@@ -5,7 +5,6 @@ interface IWikiService {
   getDescription(title: string): Promise<string>;
 }
 
-const DESCRIPTION_ENDPOINT = 'https://ja.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=';
 const THUMBNAIL_ENDPOINT = 'https://ja.wikipedia.org/api/rest_v1/page/summary/';
 @injectable()
 class WikiService implements IWikiService {
@@ -21,18 +20,14 @@ class WikiService implements IWikiService {
   }
 
   async getDescription(title: string): Promise<string> {
-    const result = await fetch(DESCRIPTION_ENDPOINT + title);
+    const result = await fetch(THUMBNAIL_ENDPOINT + title);
 
     if (!result.ok)
       return '';
 
     const data = await result.json();
 
-    for (let id in data.query.pages) {
-      return data.query.pages[id].extract;
-    }
-
-    throw new Error('No Description');
+    return data.extract;
   }
 }
 
