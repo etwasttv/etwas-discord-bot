72d73ff93985caa103d643a01b2f7fad914222d9
etwas <etwas@example.com>
2024-07-24 04:00:16 +0900

    🐛 Fix Url

--- bot/src/commands/goodwiki.ts
+++ bot/src/commands/goodwiki.ts
@@ -2,7 +2,7 @@ import { SlashCommandBuilder } from 'discord.js';
 
 import { BotCommand } from '@/types/command';
 
-const GoodWiki = 'https://ja.wikipedia.org/wiki/%E7%89%B9%E5%88%A5:%E3%82%AB%E3%83%86%E3%82%B4%E3%83%AA%E5%86%85%E3%81%8A%E3%81%BE%E3%81%8B%E3%81%9B%E8%A1%A8%E7%A4%BA/%E7%A7%80%E9%80%B8%E3%81%AA%E8%A8%98%E4%BA%8B';
+const GoodWiki = 'https://ja.wikipedia.org/wiki/%E7%89%B9%E5%88%A5:%E3%82%AB%E3%83%86%E3%82%B4%E3%83%AA%E5%86%85%E3%81%8A%E3%81%BE%E3%81%8B%E3%81%9B%E8%A1%A8%E7%A4%BA/%E8%89%AF%E8%B3%AA%E3%81%AA%E8%A8%98%E4%BA%8B';
 
 const command: BotCommand = {
   builder: new SlashCommandBuilder()
