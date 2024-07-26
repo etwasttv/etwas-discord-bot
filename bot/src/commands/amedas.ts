774144ce71a3ba8a19fb1cc932bb348791b8e973
etwas <etwas@example.com>
2024-07-26 21:47:15 +0900

    ✨ amedas ranking from yahoo

--- bot/src/commands/amedas.ts
+++ bot/src/commands/amedas.ts
@@ -1,7 +1,13 @@
 import { BotCommand } from '@/types/command';
 import { CommandInteractionOptionResolver, SlashCommandBuilder } from 'discord.js';
-import { parse } from 'csv-parse';
-import axios from 'axios';
+import { parse } from 'node-html-parser';
+
+const RankingCategory = {
+  HighTemperature: 'HIGH_TEMPERATURE',
+  LowTemperature: 'LOW_TEMPERATURE',
+  Precipitation: 'PRECIPITATION',
+  SnowfallAmount: 'SNOWFALL_AMOUNT',
+};
 
 const command: BotCommand = {
   builder: new SlashCommandBuilder()
@@ -12,127 +18,113 @@ const command: BotCommand = {
         .setDescription('select category')
         .setRequired(true)
         .setChoices(
-          { name: '最高気温', value: 'max_temp' },
-          { name: '最低気温', value: 'min_temp' },
+          { name: '最高気温', value: RankingCategory.HighTemperature },
+          { name: '最低気温', value: RankingCategory.LowTemperature },
+          { name: '降水量', value: RankingCategory.Precipitation },
+          { name: '積雪量', value: RankingCategory.SnowfallAmount },
         )),
   handler: async interaction => {
     if (interaction.user.bot)
       return;
 
     const options = interaction.options as CommandInteractionOptionResolver;
-    const category = options.getString('category');
+    const category = options.getString('category') as Categories;
+    if (!category) {
+      await interaction.reply({ content: 'カテゴリが見つかりませんでした.', ephemeral: true });
+      return;
+    }
     await interaction.deferReply();
-    try {
-      if (category === 'max_temp') {
-        const csvRecords = await getMaxTempRanking();
 
-        let ranking = 1;
-        let before = NaN;
-        const records = csvRecords.map((r, idx) => {
-          if (r[9] !== before)
-            ranking = idx+1;
-          before = r[9];
-          return {
-            ranking,
-            point: r[2].split('（')[0],
-            region: r[1],
-            value: r[9],
-            time: `${r[11].toString().padStart(2, '0')}:${r[12].toString().padStart(2, '0')}`
-          };
-        });
+    const records = await getAmedasRankingFromYahoo(category);
 
-        await interaction.editReply(getRankingText('今日の最高気温ランキング', records, '℃'));
-      }
-      else if (category === 'min_temp') {
-        const csvRecords = await getMinTempRanking();
-
-        let ranking = 1;
-        let before = NaN;
-        const records = csvRecords.map((r, idx) => {
-          if (r[9] !== before)
-            ranking = idx+1;
-          before = r[9];
-          return {
-            ranking,
-            point: r[2].split('（')[0],
-            region: r[1],
-            value: r[9],
-            time: `${r[11].toString().padStart(2, '0')}:${r[12].toString().padStart(2, '0')}`
-          };
-        });
-
-        await interaction.editReply(getRankingText('今日の最低気温ランキング', records, '℃'));
-      }
-    } catch {
-      await interaction.editReply('アメダスの情報を取得できませんでした');
+    if (records.length === 0) {
+      await interaction.editReply(`${getCategoryString(category)}が観測された地点はありません．`);
+      return;
     }
+
+    const maxPointLength = records.reduce((prev, record) => Math.max(prev, record.point.length), 0);
+
+    const unit = '';
+    let ranking = `**今日の${getRankingTitle(category)}ランキング**\n`
+    ranking= '```\n';
+    ranking += records.map(r => `${r.rank.toString().padStart(2, ' ')}位 ${r.point.padStart(maxPointLength, '　')} ${r.value} ${unit} (${r.time})`).join('\n');
+    ranking += '\n```';
+
+    await interaction.editReply(ranking);
   }
 }
 
-type rankingRecord = {
-  ranking: number;
+type RankingRecord = {
+  rank: number;
   point: string;
-  region: string;
+  value: string;
   time: string;
-  value: number;
-}
-function getRankingText(title: string, records: rankingRecord[], unit: string) {
-  const maxPointNameLength = records.reduce((max, r) => Math.max(max, r.point.length), 0);
-  const maxRegionNameLength = records.reduce((max, r) => Math.max(max, r.region.length), 0);
-  let rows = [`**${title}**`, '```'];
-  for (const record of records) {
-    rows.push(`${record.ranking.toString().padStart(2)}位  ${record.point.padEnd(maxPointNameLength, '　')} ${record.region.padEnd(maxRegionNameLength, '　')}  ${record.value.toFixed(1).padStart(4)}${unit} (${record.time})`);
-  }
-  rows.push('```');
-  return rows.join('\n');
 }
 
-async function getMinTempRanking(top = 10) {
-  const URL = 'https://www.data.jma.go.jp/stats/data/mdrr/tem_rct/alltable/mntemsadext00_rct.csv';
-  const response = await axios.get(URL, {
-    responseType: 'arraybuffer',
-      transformResponse: data => new TextDecoder("shift-jis").decode(data),
-  });
-  const parser = parse(response.data, {
-    on_record: (record, context) => context.lines === 1 ? null : record,
-    cast: (value, context) => {
-      if (!(context.index === 1 || context.index === 2))
-        return Number(value);
-      return value;
-    }
-  });
-  type schema = [
-    number,string,string,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,
-  ];
-  const records: schema[] = [];
-  for await (const record of parser) {
-    records.push(record);
-  }
-  return records.sort((a, b) => a[9] - b[9]).slice(0, top);
+type Categories = 'HIGH_TEMPERATURE'|'LOW_TEMPERATURE'|'PRECIPITATION'|'SNOWFALL_AMOUNT';
+
+const baseUrl = 'https://weather.yahoo.co.jp/weather/amedas/ranking/?rank=';
+
+async function getAmedasRankingFromYahoo(category: Categories): Promise<RankingRecord[]> {
+  const url = baseUrl + getQueryParams(category);
+  const page = await fetch(url);
+  const html = parse(await page.text());
+  const table = html.getElementById('yjw_kakuchi')?.getElementsByTagName('table')[0];
+  const rows = table?.childNodes.slice(2);
+  if (!table || !rows)
+    return [];
+
+  return rows
+    .map(r => r.childNodes
+      .map(c => c.text.replace(/<.*?>/g, '').trim())
+      .filter(s => s.length > 0)
+      .filter(r => r.length > 0))
+    .filter(r => r.length > 0)
+    .map(row => <RankingRecord>{
+      rank: Number(row[0]),
+      point: row[1],
+      value: row[2],
+      time: row[3],
+    });
 }
 
-async function getMaxTempRanking(top = 10) {
-  const URL = 'https://www.data.jma.go.jp/stats/data/mdrr/tem_rct/alltable/mxtemsadext00_rct.csv';
-  const response = await axios.get(URL, {
-    responseType: 'arraybuffer',
-      transformResponse: data => new TextDecoder("shift-jis").decode(data),
-  });
-  const parser = parse(response.data, {
-    on_record: (record, context) => context.lines === 1 ? null : record,
-    cast: (value, context) => {
-      if (!(context.index === 1 || context.index === 2))
-        return Number(value);
-      return value;
-    }
-  });
-  type schema = [
-    number,string,string,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,number,
-  ];
-  const records: schema[] = [];
-  for await (const record of parser) {
-    records.push(record);
+function getRankingTitle(category: Categories): string {
+  switch(category) {
+    case 'HIGH_TEMPERATURE':
+      return '最高気温';
+    case 'LOW_TEMPERATURE':
+      return '最低気温';
+    case 'PRECIPITATION':
+      return '降水量';
+    case 'SNOWFALL_AMOUNT':
+      return '積雪量';
   }
-  return records.sort((a, b) => a[9] - b[9]).reverse().slice(0, top);
+}
+
+function getCategoryString(category: Categories): string {
+  switch(category) {
+    case 'HIGH_TEMPERATURE':
+      return '気温';
+    case 'LOW_TEMPERATURE':
+      return '気温';
+    case 'PRECIPITATION':
+      return '降水';
+    case 'SNOWFALL_AMOUNT':
+      return '積雪';
+  }
+}
+
+function getQueryParams(category: Categories): string {
+  switch(category) {
+    case 'HIGH_TEMPERATURE':
+      return 'high_temp';
+    case 'LOW_TEMPERATURE':
+      return 'low_temp';
+    case 'PRECIPITATION':
+      return 'precip';
+    case 'SNOWFALL_AMOUNT':
+      return 'snow';
+  }
 }
 
 export default command;
