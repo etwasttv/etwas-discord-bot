677f636242e05774c70c272b34cdfea58c5876e1
etwas <etwas@example.com>
2025-01-04 23:07:24 +0900

    ✨ 素因数分解

--- bot/src/commands/sosu.ts
+++ bot/src/commands/sosu.ts
@@ -1,22 +1,55 @@
 import { BotCommand } from '@/types/command';
 import { CommandInteractionOptionResolver, SlashCommandBuilder } from 'discord.js';
 
-type SosuJudge = {
-  isSosu: boolean;
-  dividedBy?: number;
-  error?: string;
-}
+const ue = '⁰¹²³⁴⁵⁶⁷⁸⁹';
 
-function judge(number: number): SosuJudge {
-  if (number <= 1) return { isSosu: false, error: '入力値が不正' };
-  if (number === 2) return { isSosu: true };
-  if (number%2 === 0) return { isSosu: false, dividedBy: 2 };
+function judge(number: number): boolean {
+  if (number <= 1) return false;
+  if (number === 2) return true;
+  if (number%2 === 0) return false;
   let d = 3;
   while (d <= Math.sqrt(number)) {
-    if (number%d === 0) return { isSosu: false, dividedBy: d };
+    if (number%d === 0) return false;
     d += 2;
   }
-  return { isSosu: true };
+  return true;
+}
+
+function factorize(number: number): Array<number> {
+  if (number < 1) return [];
+  let x = number;
+  const result: Array<number> = [];
+  if (number == 1 || judge(number)) {
+    result.push(number);
+    return result;
+  }
+  while (x%2 === 0) {
+    x /= 2;
+    result.push(2);
+  }
+  let d = 3;
+  while (d <= x) {
+    while (x%d === 0) {
+      x /= d;
+      result.push(d);
+    }
+    d += 2;
+  }
+  return result;
+}
+
+function generateUe(value: number): string {
+  let result = '';
+  let x = value;
+
+  while (x > 0) {
+    const i = x%10;
+    result = ue.charAt(i) + result;
+    x /= 10;
+    x = Math.floor(x);
+  }
+
+  return result;
 }
 
 const command: BotCommand = {
@@ -43,22 +76,29 @@ const command: BotCommand = {
 
     await interaction.deferReply();
 
-    const isSosu = judge(number);
-    if (isSosu.isSosu) {
+    if (judge(number)) {
       await interaction.editReply({
         content: `${number} は素数です！`,
       });
       return;
     }
-    else if (isSosu.dividedBy) {
+    else {
+      const factor = factorize(number);
+      const summary = new Map<number, number>();
+      for (const f of factor) {
+        const count = summary.get(f) ?? 0;
+        summary.set(f, count+1);
+      }
+      let formulas: string[] = [];
+      for (const count of summary) {
+        formulas.push(`${count[0]}${generateUe(count[1])}`);
+      }
+      let msg = `${number} は素数じゃありません！\n`;
+      msg += `${formulas.join('x')} = ${number} なので合成数です！`;
       await interaction.editReply({
-        content: `${number} は素数ではありません！ ${isSosu.dividedBy} で割ることができます`,
+        content: msg,
       });
-      return;
     }
-    await interaction.editReply({
-      content: `${number} が素数かどうか判定できませんでした`,
-    });
   },
 };
 
